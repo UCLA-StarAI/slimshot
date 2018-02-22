9738d916d054fb9fa2d290c8573bfa249d77bb61
Tal Friedman <talf301@gmail.com>
2018-02-22 15:15:35 -0800

    No longer capping values... it works???

--- safesample/sql_aggregates.sql
+++ safesample/sql_aggregates.sql
@@ -1,9 +1,6 @@
 -- from Oblivious Bounds on the Probability of Boolean Functions 2014, Gatterbauer and Suciu
 create or replace function ior_sfunc (double precision, double precision) returns double precision as
-'select max(val) from (
-VALUES($1 * (1.0 - $2)),
-(0.00001))
-AS Vals(val)'
+'VALUES($1 * (1.0 - $2))'
 language SQL;
 
 create or replace function ior_finalfunc (double precision) returns double precision as
